18238015fc67a277907c5ffab7589c2d18904c6a
offeck <offeck@gmail.com>
2026-01-15 13:26:00 +0000

    Refactor NewsFeedServerMain to comment out server initialization code

--- work_3/server/src/main/java/bgu/spl/net/impl/newsfeed/NewsFeedServerMain.java
+++ work_3/server/src/main/java/bgu/spl/net/impl/newsfeed/NewsFeedServerMain.java
@@ -7,20 +7,20 @@ import bgu.spl.net.srv.Server;
 public class NewsFeedServerMain {
 
     public static void main(String[] args) {
-        NewsFeed feed = new NewsFeed(); //one shared object
-
-        // you can use any server... 
-        Server.threadPerClient(
-                7777, //port
-                () -> new RemoteCommandInvocationProtocol<>(feed), //protocol factory
-                ObjectEncoderDecoder::new //message encoder decoder factory
-        ).serve();
-
-        // Server.reactor(
-        //         Runtime.getRuntime().availableProcessors(),
-        //         7777, //port
-        //         () ->  new RemoteCommandInvocationProtocol<>(feed), //protocol factory
-        //         ObjectEncoderDecoder::new //message encoder decoder factory
+        // NewsFeed feed = new NewsFeed(); //one shared object
+        //
+        // // you can use any server...
+        // Server.threadPerClient(
+        // 7777, //port
+        // () -> new RemoteCommandInvocationProtocol<>(feed), //protocol factory
+        // ObjectEncoderDecoder::new //message encoder decoder factory
         // ).serve();
+        //
+        // // Server.reactor(
+        // // Runtime.getRuntime().availableProcessors(),
+        // // 7777, //port
+        // // () -> new RemoteCommandInvocationProtocol<>(feed), //protocol factory
+        // // ObjectEncoderDecoder::new //message encoder decoder factory
+        // // ).serve();
     }
 }
